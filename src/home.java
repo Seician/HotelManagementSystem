75c3cfeee49dc68a9cab416e3ddc28ad33e1b691
Seician <seician09@yahoo.com>
2021-11-12 22:23:15 +0200

    Terminare pagina CustomerDetailsBill

--- src/home.java
+++ src/home.java
@@ -1,5 +1,6 @@
 
 import javax.swing.JOptionPane;
+import project.CustomerDetailsBill;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -137,6 +138,7 @@ public class home extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        new CustomerDetailsBill().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
